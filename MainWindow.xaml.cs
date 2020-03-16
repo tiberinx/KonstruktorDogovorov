56bc2e15fcc26da71dc4f26528d1f2fe6c0e6238
tiberinx <tiberinx@gmail.com>
2020-03-16 22:35:14 +0300

    файл читается, но переменную docvar не видит. Не могу понять почему, не гуглится нифига

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Word = Microsoft.Office.Interop.Word;
+using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml.Packaging;
 
 namespace KonstruktorDogovorov
 {
@@ -26,16 +27,44 @@ namespace KonstruktorDogovorov
             InitializeComponent();
         }
 
+        public static DocumentVariable GetVariableByName(string name, WordprocessingDocument document)
+        {
+            
+            DocumentSettingsPart documentSettings = document.MainDocumentPart.DocumentSettingsPart;
+         
+            Settings settings = documentSettings.Settings;
+         
+            DocumentVariables variables = settings.Elements<DocumentVariables>().FirstOrDefault();
+         
+            if (variables != null)
+            {
+                return variables.Elements<DocumentVariable>().Where(v => v.Name == name)
+                    .FirstOrDefault();
+            }
+            return null;
+        }
+
+     
+        public static void SetDocumentVariableValue(DocumentVariable variable, string value)
+        {
+            variable.Val = value;
+        }
+
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string CustomValue = "такие дела";
-            Word.Application app = new Word.Application();
-            Word.Document doc = app.Documents.Add(@"C:/Users/tiberinx.DESKTOP-7QBI1PE/source/repos/KonstruktorDogovorov/input.docx");
-            doc.Variables["DogovorNumber"].Value = CustomValue;
-            doc.Fields.Update();
-            doc.Save();
-            doc.Close();
-            app.Quit();
+            string path = @"C:\test\input\input.docx";
+
+            using (WordprocessingDocument document = WordprocessingDocument.Open(path, true))
+            {
+                DocumentVariable variable = GetVariableByName("DogovorNumber", document);
+                if (variable != null)
+                    SetDocumentVariableValue(variable, "New Value");
+            
+                variable.Val = "New Value"; //если вывести так,без проверки, при отладке выдает что 
+                document.SaveAs(@"D:/out.docx");
+
+            }
         }
     }
 }
