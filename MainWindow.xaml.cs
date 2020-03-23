a1d3de4ab92007b27c7a8ddb188958917a82e5e9
tiberinx <tiberinx@gmail.com>
2020-03-24 00:58:58 +0300

    Возможность добавлять переменные реализовано. Текущая задача: освоение WPF, чтобы связать элементы формы с переменными

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,59 +12,25 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using DocumentFormat.OpenXml.Wordprocessing;
-using DocumentFormat.OpenXml.Packaging;
 
 namespace KonstruktorDogovorov
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        public static DocumentVariable GetVariableByName(string name, WordprocessingDocument document)
-        {
-            
-            DocumentSettingsPart documentSettings = document.MainDocumentPart.DocumentSettingsPart;
-         
-            Settings settings = documentSettings.Settings;
-         
-            DocumentVariables variables = settings.Elements<DocumentVariables>().FirstOrDefault();
-         
-            if (variables != null)
-            {
-                return variables.Elements<DocumentVariable>().Where(v => v.Name == name)
-                    .FirstOrDefault();
-            }
-            return null;
-        }
-
-     
-        public static void SetDocumentVariableValue(DocumentVariable variable, string value)
-        {
-            variable.Val = value;
-        }
-
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string path = @"C:\test\input\input.docx";
-
-            using (WordprocessingDocument document = WordprocessingDocument.Open(path, true))
-            {
-                DocumentVariable variable = GetVariableByName("DogovorNumber", document);
-                if (variable != null)
-                    SetDocumentVariableValue(variable, "New Value");
-            
-                variable.Val = "New Value"; //если вывести так,без проверки, при отладке выдает что 
-                document.SaveAs(@"D:/out.docx");
-
-            }
+            new GeneratedClass().CreatePackage(@"D:\Output.docx");
         }
+
     }
 }
